156f612176e233e1923b8c157ec2a197580d027e
Luís Fernando Guedes <lfernandoguedes@gmail.com>
2016-06-22 01:02:32 -0300

    Adds in index exports Cinespaço and Cinesystem

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ let path = require('path');
 
 // Require other classes, files or configs
 let CinemarkCrawler = require(path.join(__dirname, 'modules', 'cinemark.crawler.class'));
+let CinespacoCrawler = require(path.join(__dirname, 'modules', 'cinespaco.crawler.class'));
+let CinesystemCrawler = require(path.join(__dirname, 'modules', 'cinesystem.crawler.class'));
 
 module.exports = {
+    CinemarkCrawler: new CinemarkCrawler(),
+    CinespacoCrawler: new CinespacoCrawler(),
     CinemarkCrawler: new CinemarkCrawler()
 }
