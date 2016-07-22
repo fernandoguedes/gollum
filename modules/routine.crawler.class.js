073a4a4fece497d5b58a1b533a8804a5333ab88a
Luís Fernando Guedes <lfernandoguedes@gmail.com>
2016-07-22 00:20:41 -0300

    Functional version of the Crawler routine
    Serialized requests closes #7
    
    Uses insertMany mongoose method to insert various records concurrenctly Closes #8

--- modules/routine.crawler.class.js
+++ modules/routine.crawler.class.js
@@ -3,23 +3,29 @@
 // Require node_modules dependencies
 const path = require('path');
 const fs = require('fs');
+const _ = require('lodash');
+const async = require('async');
 
 // Require other classes, files or configs
 const MainCrawler = require(path.join(__dirname, '../modules', 'main.crawler.class'));
 const Crawlers = require(path.join(__dirname, '../', 'index'));
+const Mongoose = require(path.join(__dirname, '../db/', 'mongoose.conf'));
+const schedulesSchema = require(path.join(__dirname, '../db/schemas/', 'schedule'));
 
 module.exports = class RoutineCrawler extends MainCrawler {
 
     start() {
         return new Promise((resolve, reject) => {
             let urlsArr = super.getUrlsFromFiles();
+
             this.createQueue(urlsArr)
+                .then(this.doRequests)
+                .then(this.saveResults)
                 .then((json) => {
                     return resolve(json);
                 })
                 .catch((err) => {
-                    console.log(err);
-                    console.log('capimzeira');
+                    return reject(err);
                 });
         });
     }
@@ -27,17 +33,51 @@ module.exports = class RoutineCrawler extends MainCrawler {
     createQueue(urlsArr) {
         return new Promise((resolve, reject) => {
             let requestArr = [];
+
             urlsArr.forEach((urlObj) => {
-                requestArr.push(Crawlers.CinesystemCrawler.getSchedule(urlObj.url));
+                switch (urlObj.cinema) {
+                    case 'cinesystem':
+                        requestArr.push(Crawlers.CinesystemCrawler.getSchedule(urlObj.url));
+                        break;
+                    case 'cinespaco':
+                        requestArr.push(Crawlers.CinespacoCrawler.getSchedule(urlObj.url));
+                        break;
+                }
             });
 
-            Promise.all(requestArr)
-                .then((json) => {
-                    return resolve(json);
-                })
-                .catch((err) => {
-                    return reject(err);
+            return resolve(requestArr);
+        });
+    }
+
+    doRequests(requestArr) {
+        return new Promise((resolve, reject) => {
+            let resultsArr = [];
+
+            async.each(requestArr, function(item, callback) {
+                item.then(function(val) {
+                    resultsArr.push(val);
+                    callback();
                 });
+            }, function(err) {
+                if (err) {
+                    return reject(err);
+                }
+
+                return resolve(resultsArr);
+            });
+        });
+    }
+
+    saveResults(arrResults) {
+        return new Promise((resolve, reject) => {
+
+            schedulesSchema.insertMany(arrResults, (err, docs) => {
+              if (err) {
+                  return reject(err);
+              }
+
+              return resolve(docs);
+            });
         });
     }
 
