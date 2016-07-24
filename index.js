e79ef3734933deaae0051865bfba255edf8382d4
Luís Fernando Guedes <lfernandoguedes@gmail.com>
2016-07-24 18:29:42 -0300

    Adds routine crawler to npm package

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ let path = require('path');
 let CinemarkCrawler = require(path.join(__dirname, 'modules', 'cinemark.crawler.class'));
 let CinespacoCrawler = require(path.join(__dirname, 'modules', 'cinespaco.crawler.class'));
 let CinesystemCrawler = require(path.join(__dirname, 'modules', 'cinesystem.crawler.class'));
+let RoutineCrawler = require(path.join(__dirname, 'modules', 'routine.crawler.class'));
 
 module.exports = {
+    RoutineCrawler: new RoutineCrawler(),
     CinemarkCrawler: new CinemarkCrawler(),
     CinespacoCrawler: new CinespacoCrawler(),
     CinesystemCrawler: new CinesystemCrawler()
